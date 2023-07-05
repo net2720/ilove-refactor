e6adc7d2b60160559ab95a4ec5e8b870c17edf37
정민규 <wjalsrb0923@gmail.com>
2023-07-05 14:25:17 +0900

    Rename : 폴더명 및 변수명 변경

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -2,10 +2,7 @@
 
 export const colors = {
   primary: "#00AD5C",
-  BtnborderOut: "#00954f",
   InputBorderInFont: "#d9d9d9",
   InputBorderOut: "#b2b2b2",
   fontColor: "#121212",
 };
-
-export default colors;
