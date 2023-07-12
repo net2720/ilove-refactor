87431c6cbd40a4563056cd470a2e5c97dc26b98c
[SW4/2반] 노은탁 <popcron13@gmail.com>
2023-07-12 20:47:41 +0900

    !HOTFIX / 배포 문제 해결 관련 원복 커밋입니다.

--- src/services/Fetcher.ts
+++ src/services/Fetcher.ts
@@ -5,7 +5,8 @@ const token = () => {
 };
 
 export const instance = axios.create({
-  baseURL: 'http://125.188.246.81:5000/api',
+  baseURL: 'http://localhost:5000/api',
+  //http://125.188.246.81:5000/api
 });
 
 instance.interceptors.request.use(
