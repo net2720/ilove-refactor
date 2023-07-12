5f5a30f6b27b6a9087a9f5162753bec3b6c42138
[SW4/2반] 노은탁 <popcron13@gmail.com>
2023-07-12 20:19:36 +0900

    feat / 병원 상세 페이지 연동 완료 커밋입니다.

--- src/services/Fetcher.ts
+++ src/services/Fetcher.ts
@@ -1,11 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
 const token = () => {
-  return localStorage.getItem("token");
+  return localStorage.getItem('token');
 };
 
 export const instance = axios.create({
-  baseURL: "http://125.188.246.81:5000/api",
+  baseURL: 'http://localhost:5000/api',
+  /*localhost/125.188.246.81*/
 });
 
 instance.interceptors.request.use(
