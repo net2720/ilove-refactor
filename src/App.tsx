80efa5052b2997dcbd6923e8ca32739ec4e973aa
정민규 <wjalsrb0923@gamil.com>
2023-07-13 14:19:47 +0900

    Feat : 리코일 기초세팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import { RecoilRoot } from "recoil";
 import {
   Home,
   SearchHp,
@@ -9,82 +10,84 @@ import {
   SignUp,
   HospitalInfo,
   MyPage,
-} from './pages/Index';
+} from "./pages/Index";
 
 const App: React.FC = () => {
   return (
     <>
-      <Container>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Helmet>
-                  <title>아이사랑</title>
-                </Helmet>
-                <Home />
-              </>
-            }
-          ></Route>
-          <Route
-            path="/myPage"
-            element={
-              <>
-                <Helmet>
-                  <title>아이사랑 - 내정보</title>
-                </Helmet>
-                <MyPage />
-              </>
-            }
-          ></Route>
-          <Route
-            path="/hospitalInfo"
-            element={
-              <>
-                <Helmet>
-                  <title>아이사랑 - 병원정보</title>
-                </Helmet>
-                <HospitalInfo />
-              </>
-            }
-          ></Route>
-          <Route
-            path="/searchHp"
-            element={
-              <>
-                <Helmet>
-                  <title>아이사랑 - 병원찾기</title>
-                </Helmet>
-                <SearchHp />
-              </>
-            }
-          ></Route>
-          <Route
-            path="/login"
-            element={
-              <>
-                <Helmet>
-                  <title>아이사랑 - 로그인</title>
-                </Helmet>
-                <Login />
-              </>
-            }
-          ></Route>
-          <Route
-            path="/signUp"
-            element={
-              <>
-                <Helmet>
-                  <title>아이사랑 - 회원가입</title>
-                </Helmet>
-                <SignUp />
-              </>
-            }
-          ></Route>
-        </Routes>
-        <NavigationBar />
-      </Container>
+      <RecoilRoot>
+        <Container>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Helmet>
+                    <title>아이사랑</title>
+                  </Helmet>
+                  <Home />
+                </>
+              }
+            ></Route>
+            <Route
+              path="/myPage"
+              element={
+                <>
+                  <Helmet>
+                    <title>아이사랑 - 내정보</title>
+                  </Helmet>
+                  <MyPage />
+                </>
+              }
+            ></Route>
+            <Route
+              path="/hospitalInfo"
+              element={
+                <>
+                  <Helmet>
+                    <title>아이사랑 - 병원정보</title>
+                  </Helmet>
+                  <HospitalInfo />
+                </>
+              }
+            ></Route>
+            <Route
+              path="/searchHp"
+              element={
+                <>
+                  <Helmet>
+                    <title>아이사랑 - 병원찾기</title>
+                  </Helmet>
+                  <SearchHp />
+                </>
+              }
+            ></Route>
+            <Route
+              path="/login"
+              element={
+                <>
+                  <Helmet>
+                    <title>아이사랑 - 로그인</title>
+                  </Helmet>
+                  <Login />
+                </>
+              }
+            ></Route>
+            <Route
+              path="/signUp"
+              element={
+                <>
+                  <Helmet>
+                    <title>아이사랑 - 회원가입</title>
+                  </Helmet>
+                  <SignUp />
+                </>
+              }
+            ></Route>
+          </Routes>
+          <NavigationBar />
+        </Container>
+      </RecoilRoot>
     </>
   );
 };
